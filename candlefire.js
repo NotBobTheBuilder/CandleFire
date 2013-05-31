8ce65e598c8c6c5c5d026fe69b8f8461d58282a6
Jack Wearden <jack@jackwearden.co.uk>
2013-05-31 20:14:15 +0100

    change URL params to camelcase

--- candlefire.js
+++ candlefire.js
@@ -9,9 +9,9 @@ var app = express();
 app.get('/', cfm.root.get);
 
 app.get('/rooms', cfm.rooms.get);
-app.get('/rooms/:roomid', cfm.rooms.get);
+app.get('/rooms/:roomId', cfm.rooms.get);
 
 app.get('/slots', cfm.slots.get);
-app.get('/slots/:slotid', cfm.slots.get);
+app.get('/slots/:slotId', cfm.slots.get);
 
 app.listen(3000);
