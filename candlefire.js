d614cda12321246d7f2fcb7ccd67262af85acbf1
Jack Wearden <jack@jackwearden.co.uk>
2013-06-02 18:14:57 +0100

    refactoring slots and rooms

--- candlefire.js
+++ candlefire.js
@@ -1,19 +1,18 @@
 var express = require('express');
 var cfm = {};
     cfm.root = require('./lib/root');
-    cfm.rooms = require('./lib/rooms');
-    cfm.slots = require('./lib/slots');
     cfm.talks = require('./lib/talks');
+    cfm.getters = require('./lib/getters');
 
 var app = express();
 
 app.get('/', cfm.root.get);
 
-app.get('/rooms', cfm.rooms.getMany);
-app.get('/rooms/:roomId', cfm.rooms.getOne);
+app.get('/rooms', cfm.getters.getMany('room'));
+app.get('/rooms/:roomId', cfm.getters.getOne('room'));
 
-app.get('/slots', cfm.slots.getMany);
-app.get('/slots/:slotId', cfm.slots.getOne);
+app.get('/slots', cfm.getters.getMany('slot'));
+app.get('/slots/:slotId', cfm.getters.getOne('slot'));
 
 app.get('/talks', cfm.talks.getMany);
 app.get('/talks/:talkId', cfm.talks.getOne);
