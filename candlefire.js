de749ed1b58c91a187e2598d064859dc7dd4efd0
Jack Wearden <jack@jackwearden.co.uk>
2013-06-02 21:07:24 +0100

    Massive refactoring effort (No functionality change)

--- candlefire.js
+++ candlefire.js
@@ -1,20 +1,20 @@
 var express = require('express');
+var app = express();
+
 var cfm = {};
     cfm.root = require('./lib/root');
     cfm.talks = require('./lib/talks');
     cfm.getters = require('./lib/getters');
 
-var app = express();
-
 app.get('/', cfm.root.get);
 
-app.get('/rooms', cfm.getters.getMany('room'));
-app.get('/rooms/:roomId', cfm.getters.getOne('room'));
+app.get('/rooms', cfm.getters.getMany('room'), cfm.getters.send);
+app.get('/rooms/:roomId', cfm.getters.getOne('room'), cfm.getters.send);
 
-app.get('/slots', cfm.getters.getMany('slot'));
-app.get('/slots/:slotId', cfm.getters.getOne('slot'));
+app.get('/slots', cfm.getters.getMany('slot'), cfm.getters.send);
+app.get('/slots/:slotId', cfm.getters.getOne('slot'), cfm.getters.send);
 
-app.get('/talks', cfm.talks.getMany);
-app.get('/talks/:talkId', cfm.talks.getOne);
+app.get('/talks', cfm.getters.getMany('talk'), cfm.getters.superimpose(["slot", "room"]), cfm.getters.send);
+app.get('/talks/:talkId', cfm.getters.getOne('talk'), cfm.getters.superimpose(["slot", "room"]), cfm.getters.send);
 
 app.listen(3000);
